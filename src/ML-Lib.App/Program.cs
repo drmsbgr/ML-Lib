0159b4ccb233e5607a96a1ce3f2b158b49d1114a
drmsbgr <drmsbgr@gmail.com>
2025-11-15 18:04:04 +0300

    app added

---
+++ src/ML-Lib.App/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using ML_Lib.Algorithms.Classification;
+using ML_Lib.Common;
+
+var folderPath = Assembly.GetExecutingAssembly().Location;
+
+var filePath = Path.Combine(folderPath, "..", "..", "..", "..", "data.txt");
+
+var dr = new DataReader(filePath);
+
+var (data, classes) = dr.Read();
+
+//knn
+var knn = new KNN(data, classes, KNNOptions.Default);
+var input = VectorFactory.Create(1.0, 0.0);
+var predictedClass = knn.Predict(input);
+
+Console.Write($"Input: {input}\nPredictedClass: {predictedClass}");
